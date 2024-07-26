7d685ce47b32612bc90b83e3e5e5b87e01f19c61
Holbiwan <sabrinapapeau@gmail.com>
2024-07-26 13:30:06 +0200

    optimise the security

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
-/* eslint-disable */
-import router from './routes/index';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import controllerRouting from './routes/index';
 
-const express = require('express');
+/**
+ * This project is a summary of back-end concepts:
+ * authentication, NodeJS, MongoDB, Redis,
+ * pagination and background processing.
+ *
+ * The objective was to build a simple platform to upload and view files:
+ *
+ * User authentication via a token
+ * List all files
+ * Upload a new file
+ * Change permission of a file
+ * View a file
+ * Generate thumbnails for images
+ */
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-router(app);
+// Middleware for security headers
+app.use(helmet());
+
+// Middleware for logging HTTP requests
+app.use(morgan('combined'));
+
+// Middleware for CORS
+app.use(cors());
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Route handling
+controllerRouting(app);
+
+// Middleware for handling errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
