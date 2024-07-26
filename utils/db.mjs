e604d0dd0a2908b99e12d7870a7322669939ad4c
Holbiwan <sabrinapapeau@gmail.com>
2024-07-26 12:12:45 +0200

    Use await to check asyncrones results

---
+++ utils/db.mjs
@@ -0,0 +1,42 @@
+/* eslint-disable */
+const { MongoClient } = require('mongodb');
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+        console.log('MongoDB client connected to the server');
+      } else {
+        this.db = false;
+        console.log(`MongoDB client not connected to the server: ${err.message}`);
+      }
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    if (!this.db) {
+      throw new Error('MongoDB client is not connected');
+    }
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.db) {
+      throw new Error('MongoDB client is not connected');
+    }
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
