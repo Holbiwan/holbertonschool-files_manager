bd8c38f4fcb7a80654da4624255d86c4e5ad4226
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 05:24:12 +0200

    Fix instance

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,32 +1,40 @@
 import { MongoClient } from 'mongodb';
-import { promisify } from 'util';
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
-    
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect((err) => {
+
+    const uri = `mongodb://${host}:${port}/`;
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    this.client.connect(err => {
       if (err) {
-        console.error('Error connecting to MongoDB:', err);
+        console.error('Failed to connect to MongoDB', err);
+        this.db = null;
+      } else {
+        console.log('Connected to MongoDB');
+        this.db = this.client.db(database);
       }
     });
-    
-    this.db = this.client.db(database);
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return !!this.db;
   }
 
   async nbUsers() {
+    if (!this.db) {
+      return 0;
+    }
     return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
+    if (!this.db) {
+      return 0;
+    }
     return this.db.collection('files').countDocuments();
   }
 }
