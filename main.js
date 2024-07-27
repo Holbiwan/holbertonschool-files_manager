d673a70fa429d517d72da6a8b4c451d0e01c67ec
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 03:29:05 +0200

    Update main.js file

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
-import { MongoClient } from 'mongodb';
+import redisClient from './utils/redis';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+(async () => {
+  console.log(redisClient.isAlive());
+  console.log(await redisClient.get('myKey'));
+  await redisClient.set('myKey', 12, 5);
+  console.log(await redisClient.get('myKey'));
 
-class DBClient {
-  constructor() {
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect()
-      .then((client) => {
-        this.db = client.db(DB_DATABASE);
-        console.log('MongoDB client connected to the server');
-      })
-      .catch((err) => {
-        console.error(`MongoDB client not connected to the server: ${err.message}`);
-        this.db = false;
-      });
-  }
-
-  isAlive() {
-    return !!this.db;
-  }
-
-  async nbUsers() {
-    if (!this.isAlive()) {
-      throw new Error('MongoDB client is not connected');
-    }
-    return this.db.collection('users').countDocuments();
-  }
-
-  async nbFiles() {
-    if (!this.isAlive()) {
-      throw new Error('MongoDB client is not connected');
-    }
-    return this.db.collection('files').countDocuments();
-  }
-}
-
-const dbClient = new DBClient();
-export default dbClient;
+  setTimeout(async () => {
+    console.log(await redisClient.get('myKey'));
+  }, 1000 * 10);
+})();
