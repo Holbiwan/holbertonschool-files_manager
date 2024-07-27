8473ee25c68020e913fa739b6d83c73ad698d8bf
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 18:16:22 +0200

    Use Promise instead of import

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,33 +1,39 @@
-import { createClient } from 'redis';
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    this.client = createClient({
-      url: 'redis://localhost:6379',
-    });
-
-    this.client.on('error', (err) => console.error('Redis Client Error', err));
-    this.client.connect().catch(console.error);
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client error: ${error}`));
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    return this.client.get(key);
+    return this.handleRedisOperation('get', key);
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, {
-      EX: duration,
-    });
+    return this.handleRedisOperation('setex', key, duration, value);
   }
 
   async del(key) {
-    await this.client.del(key);
+    return this.handleRedisOperation('del', key);
+  }
+
+  handleRedisOperation(method, ...args) {
+    return new Promise((resolve, reject) => {
+      this.client[method](...args, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
   }
 }
 
 const redisClient = new RedisClient();
-export default redisClient;
+module.exports = redisClient;
