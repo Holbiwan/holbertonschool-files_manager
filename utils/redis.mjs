9cf8f941378f2c047684a49b7b09e1169f94aa43
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 05:17:59 +0200

    Try to dix issues

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,35 +1,53 @@
-import { MongoClient } from 'mongodb';
+import redis from 'redis';
+import { promisify } from 'util';
 
-class DBClient {
+class RedisClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.url = `mongodb://${this.host}:${this.port}`;
-    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
-    this.db = null;
-  }
+    this.client = redis.createClient({
+      host: 'localhost',
+      port: 6379,
+      retry_strategy: options => {
+        if (options.error && options.error.code === "ECONNREFUSED") {
+          // End reconnecting on a specific error and flush all commands with
+          // a individual error
+          console.error("The server refused the connection");
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+          // End reconnecting after a specific timeout and flush all commands
+          // with a individual error
+          console.error("Retry time exhausted");
+        }
+        if (options.attempt > 10) {
+          // End reconnecting with built in error
+          console.error("Max attempts reached");
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000);
+      }
+    });
 
-  async connect() {
-    await this.client.connect();
-    this.db = this.client.db(this.database);
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return this.client.connected;
   }
 
-  async nbUsers() {
-    return await this.db.collection('users').countDocuments();
+  async get(key) {
+    return await this.getAsync(key);
   }
 
-  async nbFiles() {
-    return await this.db.collection('files').countDocuments();
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
   }
 }
 
-const dbClient = new DBClient();
-// Ensure that the dbClient is connected before exporting
-await dbClient.connect();
-
-export default dbClient;
+const redisClient = new RedisClient();
+export default redisClient;
