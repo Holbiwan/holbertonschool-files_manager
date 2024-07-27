8a18e846ce4881128e8f8063539ba283fb4843a2
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 06:21:30 +0200

    Define routes of index.js file

--- server.js
+++ server.js
@@ -1,50 +1,11 @@
 import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
-import controllerRouting from './routes/index';
-
-/**
- * This project is a summary of back-end concepts:
- * authentication, NodeJS, MongoDB, Redis,
- * pagination and background processing.
- *
- * The objective was to build a simple platform to upload and view files:
- *
- * User authentication via a token
- * List all files
- * Upload a new file
- * Change permission of a file
- * View a file
- * Generate thumbnails for images
- */
+import routes from './routes/index.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// Middleware for security headers
-app.use(helmet());
+app.use(routes);
 
-// Middleware for logging HTTP requests
-app.use(morgan('combined'));
-
-// Middleware for CORS
-app.use(cors());
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Route handling
-controllerRouting(app);
-
-// Middleware for handling errors
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-export default app;
