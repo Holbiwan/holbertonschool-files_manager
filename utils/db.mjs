1db2babd81348f68093bfdda63a79d320b2b8389
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 18:22:52 +0200

    Update after of the modification of refis file

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,45 +1,64 @@
-import mongodb from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-const { MongoClient } = mongodb;
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
+    this.db = null;
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (error) console.log(error);
+      this.db = client.db(database);
+      (async () => {
+        try {
+          const collections = await this.db.listCollections().toArray();
+          const collectionNames = collections.map((col) => col.name);
 
-    const uri = `mongodb://${host}:${port}/`;
-    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+          if (!collectionNames.includes('users')) {
+            await this.db.createCollection('users');
+          }
 
-    this.client.connect((err) => {
-      if (err) {
-        console.error('Failed to connect to MongoDB', err);
-        this.db = null;
-      } else {
-        console.log('Connected to MongoDB');
-        this.db = this.client.db(database);
-      }
+          if (!collectionNames.includes('files')) {
+            await this.db.createCollection('files');
+          }
+        } catch (err) {
+          console.error('Failed to create collections', err);
+        }
+      })();
     });
   }
 
   isAlive() {
+    // Check if MongoDB client is connected
     return !!this.db;
   }
 
   async nbUsers() {
-    if (!this.db) {
-      return 0;
-    }
+    // Count number of documents in 'users' collection
     return this.db.collection('users').countDocuments();
   }
 
+  async getUser(query) {
+    // Search for the user in the collection
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
+    return user;
+  }
+
   async nbFiles() {
-    if (!this.db) {
-      return 0;
-    }
+    // Count number of documents in 'files' collection
     return this.db.collection('files').countDocuments();
+  }
+
+  async saveFile(fileData) {
+    // Insert a new document into the 'files' collection
+    const result = await this.db.collection('files').insertOne(fileData);
+    return { _id: result.insertedId, ...fileData };
   }
 }
 
-const dbClient = new DBClient();
-export default dbClient;
+// Create and export an instance of DBClient
+module.exports = new DBClient();
