c568a47cd4f6014f41d5b6cadb0a45d42652178b
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 05:21:22 +0200

    Fix eslint errors

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -6,24 +6,24 @@ class RedisClient {
     this.client = redis.createClient({
       host: 'localhost',
       port: 6379,
-      retry_strategy: options => {
-        if (options.error && options.error.code === "ECONNREFUSED") {
+      retry_strategy: (options) => {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
           // End reconnecting on a specific error and flush all commands with
-          // a individual error
-          console.error("The server refused the connection");
+          // an individual error
+          console.error('The server refused the connection');
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
           // End reconnecting after a specific timeout and flush all commands
-          // with a individual error
-          console.error("Retry time exhausted");
+          // with an individual error
+          console.error('Retry time exhausted');
         }
         if (options.attempt > 10) {
-          // End reconnecting with built in error
-          console.error("Max attempts reached");
+          // End reconnecting with built-in error
+          console.error('Max attempts reached');
         }
         // reconnect after
         return Math.min(options.attempt * 100, 3000);
-      }
+      },
     });
 
     this.client.on('error', (err) => console.error('Redis Client Error', err));
@@ -37,7 +37,7 @@ class RedisClient {
   }
 
   async get(key) {
-    return await this.getAsync(key);
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
