f9260e7b91faefb43f783df5d35169bab5741c18
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 07:04:08 +0200

    Check class

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,39 +1,22 @@
-import redis from 'redis';
+import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient({
-      host: 'localhost',
-      port: 6379,
-      retry_strategy: (options) => {
-        if (options.error && options.error.code === 'ECONNREFUSED') {
-          // End reconnecting on a specific error and flush all commands with
-          // an individual error
-          console.error('The server refused the connection');
-        }
-        if (options.total_retry_time > 1000 * 60 * 60) {
-          // End reconnecting after a specific timeout and flush all commands
-          // with an individual error
-          console.error('Retry time exhausted');
-        }
-        if (options.attempt > 10) {
-          // End reconnecting with built-in error
-          console.error('Max attempts reached');
-        }
-        // reconnect after
-        return Math.min(options.attempt * 100, 3000);
-      },
+    this.client = createClient({
+      url: 'redis://localhost:6379'
     });
 
     this.client.on('error', (err) => console.error('Redis Client Error', err));
+    this.client.connect().catch(console.error);
+
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.client.isOpen();
   }
 
   async get(key) {
