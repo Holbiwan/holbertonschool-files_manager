a7db636b7491b282098f99304cdabb24e203a522
Holbiwan <sabrinapapeau@gmail.com>
2024-07-27 06:28:02 +0200

    Define endpoints for status ans stats

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,37 +1,17 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 class AppController {
-  /**
-   * Get the status of Redis and MongoDB clients
-   * @param {Object} request - The request object
-   * @param {Object} response - The response object
-   */
   static getStatus(req, res) {
-    const status = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive(),
-    };
-    res.status(200).json(status);
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    return res.status(200).json({ redis, db });
   }
 
-  /**
-   * Get the statistics of users and files in the database
-   * @param {Object} request - The request object
-   * @param {Object} response - The response object
-   */
   static async getStats(req, res) {
-    try {
-      const [users, files] = await Promise.all([
-        DBClient.nbUsers(),
-        DBClient.nbFiles(),
-      ]);
-      const stats = { users, files };
-      res.status(200).json(stats);
-    } catch (error) {
-      console.error('Error fetching statistics:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return res.status(200).json({ users, files });
   }
 }
 
