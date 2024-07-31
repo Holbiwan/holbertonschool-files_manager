39ca45b6acd0a78b2cdb272244c4598f42180f19
root <root@Sabrina>
2024-07-31 14:57:08 +0200

    Fix the logic of the code

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-// File: server.js
-
-import express from 'express';
-import routes from './routes/index';
+const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
-app.use(express.json())
+
+app.use(express.json());
+
 app.use(routes);
 
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
